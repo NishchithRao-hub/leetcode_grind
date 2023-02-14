82a2924966b3b1e45e003aede47f96bdc2f6b24a
Nishchith Rao <nishchithraopr@gmail.com>
2023-02-14 06:32:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (99.99%)

---
+++ my-folder/0112-path-sum/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean root2LeafSum(TreeNode root, int targetSum, int sum) {
+        if(root==null) return false;
+        if(root.left==null && root.right==null){
+            sum=sum+root.val;
+            if(sum==targetSum)
+                return true;
+        }
+        return root2LeafSum(root.left,targetSum,sum+root.val) || root2LeafSum     (root.right, targetSum, sum+root.val);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum){
+        int sum=0;
+        return root2LeafSum(root,targetSum,sum);
+    }
+}
