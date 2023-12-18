4c6ff508b617406f263b8a671433757fdae080c4
Nishchith Rao <nishchithraopr@gmail.com>
2023-12-18 23:08:53 +0000

    [LeetCode Sync] Runtime - 159 ms (5.94%), Memory - 17.8 MB (100.00%)

---
+++ my-folder/2042-maximum-product-difference-between-two-pairs/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        f_large = s_large = 0
+        f_small = s_small = float("inf")
+
+        for n in nums:
+            if n < f_small:
+                s_small , f_small = f_small, n
+            elif n < s_small:
+                s_small = n
+
+            if n > f_large:
+                s_large, f_large = f_large, n
+            elif n > s_large:    
+                s_large = n
+
+        return f_large * s_large - f_small * s_small 
