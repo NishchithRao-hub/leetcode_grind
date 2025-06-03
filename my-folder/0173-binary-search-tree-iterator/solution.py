9bdd19416c6372139a665f3d3975daaea5e03b41
Nishchith Rao <nishchithraopr@gmail.com>
2025-06-03 02:03:48 +0000

    [LeetCode Sync] Runtime - 15 ms (13.61%), Memory - 25 MB (58.63%)

--- my-folder/0173-binary-search-tree-iterator/solution.py
+++ my-folder/0173-binary-search-tree-iterator/solution.py
@@ -7,22 +7,26 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.iterator = self.inorder(root)
-        self.nxt = next(self.iterator, None)
+        self.arr = []
+        self.curr_idx = 0
+        self.inorder(root)
 
-    def inorder(self, node: Optional[TreeNode]) -> Generator[int, None, None]:
-        if node:
-            yield from self.inorder(node.left)
-            yield node.val
-            yield from self.inorder(node.right)
+    def inorder(self, root):
+        if not root:
+            return
+        self.inorder(root.left)
+        self.arr.append(root.val)
+        self.inorder(root.right)
 
     def next(self) -> int:
-        res, self.nxt = self.nxt, next(self.iterator, None)
-        return res
+        value = self.arr[self.curr_idx]
+        self.curr_idx += 1
+        return value        
 
     def hasNext(self) -> bool:
-        return self.nxt is not None
-        
+        return self.curr_idx <= len(self.arr) - 1
+
+
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
 # param_1 = obj.next()
