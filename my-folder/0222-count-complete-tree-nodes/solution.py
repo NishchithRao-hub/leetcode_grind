c9c52fae0a3c1e8007d44a9ab2cf26fc2af91c7f
Nishchith Rao <nishchithraopr@gmail.com>
2025-06-03 04:32:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.2 MB (96.34%)

---
+++ my-folder/0222-count-complete-tree-nodes/solution.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        def getHeight(node):
+            height = 0
+            while node:
+                height += 1
+                node = node.left
+            return height
+
+        if not root:
+            return 0
+
+        left_height = getHeight(root.left)
+        right_height = getHeight(root.right)
+
+        if left_height == right_height:
+            # Left subtree is a perfect binary tree
+            return (1 << left_height) + self.countNodes(root.right)
+        else:
+            # Right subtree is a perfect binary tree of height (h-1)
+            return (1 << right_height) + self.countNodes(root.left)
