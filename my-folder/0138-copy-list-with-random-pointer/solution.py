291841813a71793f4e777de5b18fdee6b8b9f5db
Nishchith Rao <nishchithraopr@gmail.com>
2025-06-15 00:53:13 +0000

    [LeetCode Sync] Runtime - 40 ms (67.03%), Memory - 18.7 MB (40.40%)

--- my-folder/0138-copy-list-with-random-pointer/solution.py
+++ my-folder/0138-copy-list-with-random-pointer/solution.py
@@ -9,23 +9,15 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
-        
-        deep_copy = {}  # Creating a hash map
+        deepCopy = collections.defaultdict(lambda: Node(0))
+        deepCopy[None] = None
 
-        # Creating new nodes and assigning their values
-        temp = head
-        while temp:
-            deep_copy[temp] = Node(temp.val)
-            temp = temp.next
+        curr = head
+        while curr:
+            deepCopy[curr].val = curr.val
+            deepCopy[curr].next = deepCopy[curr.next]
+            deepCopy[curr].random = deepCopy[curr.random]
+            curr = curr.next
 
-        # Assigning the next and random pointers of each node correctly
-        temp = head
-        while temp:
-            deep_copy[temp].next = deep_copy.get(temp.next)
-            deep_copy[temp].random = deep_copy.get(temp.random)
-            temp = temp.next
-        
-        return deep_copy[head]
+        return deepCopy[head]
         
