e3f8fb4372e84bdab4182b341c34d24f3afc8f6a
Nishchith Rao <nishchithraopr@gmail.com>
2025-06-21 19:15:19 +0000

    [LeetCode Sync] Runtime - 222 ms (97.34%), Memory - 20.2 MB (68.38%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,22 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        moves = [[1,0], [-1, 0], [0, 1], [0, -1]]
+        # DFS Approach
+        moves = [[1,0], [-1,0], [0,1], [0,-1]]
         rows, cols = len(grid), len(grid[0])
-        islands  = 0
+        islands = 0
 
-        def dfs(r, c):
-            if (r<0 or c<0 or r>=rows or c>=cols or grid[r][c] == "0"):
+        def dfs(r_index, c_index):
+            if (r_index < 0 or c_index < 0 or r_index >= rows or c_index >=
+                cols or grid[r_index][c_index] == "0"):
                 return
 
-            grid[r][c] = "0"            
+            grid[r_index][c_index] = "0"
             for dir_r, dir_c in moves:
-                dfs(r + dir_r, c + dir_c)
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    dfs(r, c)
+                dfs(r_index + dir_r, c_index + dir_c)
+            
+        for r_index in range(rows):
+            for c_index in range(cols):
+                if grid[r_index][c_index] == "1":
+                    dfs(r_index, c_index)
                     islands += 1
-
         return islands
+
+# Time complexity = O(m*n)
+# Space complexity = O(m*n)
+# Where m is the number of rows and n is the number of columns in the grid.
         
