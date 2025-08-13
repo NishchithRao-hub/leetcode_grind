207dc3100b49461d18a2661a8657f4c448162958
Nishchith Rao <nishchithraopr@gmail.com>
2025-08-13 05:59:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (84.36%)

---
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+            
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        result = [""]
+
+        for digit in digits:
+            temp = []
+            for curr in result:
+                for ch in digitToChar[digit]:
+                    temp.append(curr + ch)
+
+            result = temp
+        return result
+        
