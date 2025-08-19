82c86490392d2272b81aa659218754d03321261b
Nishchith Rao <nishchithraopr@gmail.com>
2025-08-19 05:21:44 +0000

    [LeetCode Sync] Runtime - 195 ms (76.38%), Memory - 18.1 MB (31.92%)

---
+++ my-folder/0473-matchsticks-to-square/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        # Backtracking and DFS
+        total_length = sum(matchsticks)
+        if total_length % 4 != 0:
+            return False
+
+        length = total_length // 4
+        sides = [0] * 4
+        matchsticks.sort(reverse=True)
+
+        def dfs(i):
+            if i == len(matchsticks):
+                return True
+
+            for side in range(4):
+                if sides[side] + matchsticks[i] <= length:
+                    sides[side] += matchsticks[i]
+                    if dfs(i+1):
+                        return True
+                    sides[side] -= matchsticks[i]
+
+                if sides[side] == 0:
+                    break
+
+            return False
+
+        return dfs(0)
+
+# Time -> O(4^n)
+# SPace -> O(n) for recursion stack
+        
