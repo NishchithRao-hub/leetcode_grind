ba67621e8c3a07033c80ad7e99e90d12ab79c015
Nishchith Rao <nishchithraopr@gmail.com>
2025-08-26 03:18:57 +0000

    [LeetCode Sync] Runtime - 66 ms (51.18%), Memory - 21.4 MB (14.33%)

--- my-folder/0072-edit-distance/solution.py
+++ my-folder/0072-edit-distance/solution.py
@@ -1,21 +1,22 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m = len(word1)
-        n = len(word2)
-        dp = [[0] * (n+1) for _ in range(m+1)]
+        # Bottom-up DP
+        dp = [[float('inf')] * (len(word2)+1) for _ in range(len(word1)+1)]
 
-        for i in range(1, m+1):
-            dp[i][0] = i
-        for j in range(1, n+1):
-            dp[0][j] = j
-        
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
+        for j in range(len(word2)+1):
+            dp[len(word1)][j] = len(word2) - j
+        for i in range(len(word1)+1):
+            dp[i][len(word2)] = len(word1) - i
+
+        for i in range(len(word1)-1, -1, -1):
+            for j in range(len(word2)-1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
                 else:
-                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+                    dp[i][j] = 1+min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
 
-        return dp[m][n]
+        return dp[0][0]
 
+# Time -> O(m*n)
+# Space -> O(m*n) (m is len of word2,n is len of word1)
         
