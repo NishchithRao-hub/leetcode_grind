e40c3dea0b9ff3fc5ef1a84f167d127e46c0325c
Nishchith Rao <nishchithraopr@gmail.com>
2025-09-14 02:21:33 +0000

    [LeetCode Sync] Runtime - 10 ms (93.35%), Memory - 20.5 MB (26.43%)

--- my-folder/0064-minimum-path-sum/solution.py
+++ my-folder/0064-minimum-path-sum/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        a = len(grid)
-        b = len(grid[0])
+        # Bottom up DP
+        rows, cols = len(grid), len(grid[0])
+        dp = [[float("inf")] * (cols+1) for _ in range(rows+1)]
+        dp[rows-1][cols] = 0
 
-        for i in range(1, a):
-            grid[i][0] += grid[i-1][0]
-        for i in range(1, b):
-            grid[0][i] += grid[0][i-1]
-        
-        for i in range(1, a):
-            for j in range(1, b):
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        for r in range(rows-1, -1, -1):
+            for c in range(cols-1, -1, -1):
+                dp[r][c] = grid[r][c] + min(dp[r+1][c], dp[r][c+1])
 
-        return grid[-1][-1]
+        return dp[0][0]        
+
+# Time -> O(m*n) (m=num of rows, n=num of cols)
+# Space -> O(m*n)
         
