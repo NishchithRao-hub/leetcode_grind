e65b96ef498f149d8380ac070288fa0c00c9f8cb
Nishchith Rao <nishchithraopr@gmail.com>
2025-09-15 05:59:47 +0000

    [LeetCode Sync] Runtime - 355 ms (16.04%), Memory - 42.7 MB (6.03%)

---
+++ my-folder/0239-sliding-window-maximum/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # Sliding window with heap
+        heap = []
+        output = []
+        
+        for i in range(len(nums)):
+            heapq.heappush(heap, (-nums[i], i))
+            if i >= k-1:
+                while heap[0][1] <= i-k:
+                    heapq.heappop(heap)
+                output.append(-heap[0][0])
+        return output
+
+# Time -> O(nlogn)
+# Space -> O(n)
+        
