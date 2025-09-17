6ef6f868fcce09ad3ab0111c863f3c3ac896bc88
Nishchith Rao <nishchithraopr@gmail.com>
2025-09-17 05:49:42 +0000

    [LeetCode Sync] Runtime - 91 ms (96.40%), Memory - 57.8 MB (64.58%)

--- my-folder/0380-insert-delete-getrandom-o1/solution.py
+++ my-folder/0380-insert-delete-getrandom-o1/solution.py
@@ -1,39 +1,34 @@
 import random
 class RandomizedSet:
-
+    # Hash Map + Stack soln
     def __init__(self):
-        self.val_idx = {}
+        self.numMap = {}
         self.nums = []
 
     def insert(self, val: int) -> bool:
-        if val in self.val_idx:
+        if val in self.numMap:
             return False
-        
-        self.val_idx[val] = len(self.nums)
+        self.numMap[val] = len(self.nums)
         self.nums.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.val_idx:
+        if val not in self.numMap:
             return False
-        
-        idx_remove = self.val_idx[val]
-        last_element = self.nums[-1]
-
-        self.nums[idx_remove] = last_element
-        self.val_idx[last_element] = idx_remove
+        idx = self.numMap[val]
+        last = self.nums[-1]
+        self.nums[idx] = last
+        self.numMap[last] = idx
 
         self.nums.pop()
-        del self.val_idx[val]
+        del self.numMap[val]
         return True
-
+        
     def getRandom(self) -> int:
         return random.choice(self.nums)
 
-# Operation	  Time	    Space
-# Insert	    O(1)	O(n)
-# Remove	    O(1)	O(n)
-# GetRandom	   O(1)	    O(n)
+# Time -> O(1) for each function call
+# Space -> O(n)
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
