4ad68f1db8c2b7f48194c948b24ff569ef915905
Nishchith Rao <nishchithraopr@gmail.com>
2025-10-06 02:12:10 +0000

    [LeetCode Sync] Runtime - 379 ms (62.09%), Memory - 19.7 MB (76.54%)

---
+++ my-folder/0744-network-delay-time/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Djikstra's Algorithm
+        edges = defaultdict(list)
+        for u, v, w in times:
+            edges[u].append((v, w))
+
+        visit = set()
+        minHeap = [(0, k)]
+        t = 0
+
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            if n1 in visit:
+                continue
+            visit.add(n1)
+            t = w1
+
+            for n2, w2 in edges[n1]:
+                if n2 not in visit:
+                    heapq.heappush(minHeap, (w1+w2, n2))
+        
+        return t if len(visit) == n else -1
+
+# Time -> O(ElogV)
+# Space -> O(V+E)
+        
