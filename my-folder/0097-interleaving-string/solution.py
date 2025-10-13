0ad780202fd7c9b0e7c740fd7effe74c1a7c72ae
Nishchith Rao <nishchithraopr@gmail.com>
2025-10-13 16:57:12 +0000

    [LeetCode Sync] Runtime - 51 ms (24.27%), Memory - 18 MB (56.60%)

--- my-folder/0097-interleaving-string/solution.py
+++ my-folder/0097-interleaving-string/solution.py
@@ -1,24 +1,21 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m = len(s1)
-        n = len(s2)
-        length = len(s3)
-        if m + n != length:
+        # DP Bottom-up
+        if len(s1) + len(s2) != len(s3):
             return False
         
-        dp = [[False] * (n + 1) for _ in range(m+1)]
-        dp[0][0] = True
+        dp = [[False] * (len(s2)+1) for i in range(len(s1)+1)]
+        dp[len(s1)][len(s2)] = True
 
-        for i in range(1, m+1):
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                if i < len(s1) and s1[i] == s3[i+j] and dp[i+1][j]:
+                    dp[i][j] = True
+                if j < len(s2) and s2[j] == s3[i+j] and dp[i][j+1]:
+                    dp[i][j] = True
         
-        for j in range(1, n+1):
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
+        return dp[0][0]
 
-        for i in range(1, m+1):
-            for j in range(1, n + 1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
-
-        return dp[m][n]
-
+# Time -> O(m*n)
+# Space -> O(m*n)
         
