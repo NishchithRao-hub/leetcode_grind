ca4f9a66961aa48b9065cfe9b9d19066eb5dcf90
Nishchith Rao <nishchithraopr@gmail.com>
2025-10-15 00:26:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.9 MB (33.55%)

---
+++ my-folder/0890-lemonade-change/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        # Greedy - Iteration
+        five, ten = 0, 0
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                five, ten = five-1, ten+1
+            elif ten > 0:
+                five, ten = five-1, ten-1
+            else:
+                five -= 3
+            if five < 0:
+                return False
+        return True
+
+# Time -> O(n)
+# Space -> O(1)
+        
