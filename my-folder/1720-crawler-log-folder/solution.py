86648ad445ee20fb2becfa7b383ad220827faa37
Nishchith Rao <nishchithraopr@gmail.com>
2026-01-20 01:20:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (24.09%)

---
+++ my-folder/1720-crawler-log-folder/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Iterative solution
+    def minOperations(self, logs: List[str]) -> int:
+        result = 0
+        for log in logs:
+            if log == "./":
+                continue
+            if log == "../":
+                result = max(0, result-1)
+            else:
+                result += 1
+
+        return result
+
+# Time -> O(n)
+# Space -> O(1)
+        
